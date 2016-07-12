0745238d011a58dcf287cd59af47668b469eb144
Dave Vasilevsky <dave@vasilevsky.ca>
2016-07-12 16:18:49 -0400

    Don't depend on nix for just pread/pwrite

--- src/unix.rs
+++ src/unix.rs
@@ -1,22 +1,34 @@
 use std::fs::File;
-use std::io::{Result, Write};
+use std::io::{Result, Write, Error};
 use std::os::unix::io::AsRawFd;
 use super::{ReadAt, WriteAt};
 
-extern crate nix;
-use self::nix::sys::uio;
+extern crate libc;
+use self::libc::{pread, pwrite, c_void, off_t, size_t, ssize_t};
+
+fn err(e: ssize_t) -> Result<usize> {
+    if e == -1 as ssize_t {
+        Err(Error::last_os_error())
+    } else {
+        Ok(e as usize)
+    }
+}
 
 impl ReadAt for File {
     fn read_at(&self, pos: u64, buf: &mut [u8]) -> Result<usize> {
         let fd = self.as_raw_fd();
-        uio::pread(fd, buf, pos as i64).map_err(From::from)
+        err(unsafe {
+            pread(fd, buf.as_mut_ptr() as *mut c_void, buf.len() as size_t, pos as off_t)
+        })
     }
 }
 
 impl WriteAt for File {
     fn write_at(&mut self, pos: u64, buf: &[u8]) -> Result<usize> {
         let fd = self.as_raw_fd();
-        uio::pwrite(fd, buf, pos as i64).map_err(From::from)
+        err(unsafe {
+            pwrite(fd, buf.as_ptr() as *const c_void, buf.len() as size_t, pos as off_t)
+        })
     }
     fn flush(&mut self) -> Result<()> {
         Write::flush(self)
