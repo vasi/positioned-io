ff48670221542d9c6e6694209e36cf75904bb894
Dave Vasilevsky <dave@vasilevsky.ca>
2016-07-24 23:10:49 -0400

    0.2.2

--- implementors/positioned_io/trait.WriteIntAt.js
+++ implementors/positioned_io/trait.WriteIntAt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['positioned_io'] = [];implementors['positioned_io'] = [];implementors['positioned_io'] = [];implementors['positioned_io'] = [];
+implementors["positioned_io"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
